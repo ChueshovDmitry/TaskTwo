07f5e6c79918cc24e6eef71bc53e9879b8d54141
Dmitry Chueshov <dimka-chueshov@mail.ru>
2020-05-18 23:14:34 +0300

    edit Runner

--- src/Runner.java
+++ src/Runner.java
@@ -31,11 +31,11 @@ import java.util.*;
 
 public class Runner {
     public static void main(String[] args) {
-//            String path = "src/resources/comptext.txt";
+            String path = "src/resources/comptext.txt";
+//
 //        Text currentText = new Text("D:/IdeaProj/TaskTwo/src/resources/comptext.txt");
-          Text currentText = new Text("src/resources/comptext.txt");
+          Text currentText = new Text(path);
 
-        System.out.println("hhskjfsdfsf");
 
         try{
             currentText.createtext();
