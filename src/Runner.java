a04a642ae419a60e718d1047e7aebbbc0d7236ac
Dmitry Chueshov <dimka-chueshov@mail.ru>
2020-05-18 20:17:58 +0300

    edit path file

--- src/Runner.java
+++ src/Runner.java
@@ -32,7 +32,10 @@ import java.util.*;
 public class Runner {
     public static void main(String[] args) {
 
-        Text currentText = new Text("C:\\comptext.txt");
+
+//        Text currentText = new Text("D:/IdeaProj/TaskTwo/src/resources/comptext.txt");
+          Text currentText = new Text("src/resources/comptext.txt");
+
 
         try{
             currentText.createtext();
